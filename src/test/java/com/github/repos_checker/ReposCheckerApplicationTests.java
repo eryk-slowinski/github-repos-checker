e16af3a15d15bd8671740d65666bafafcd0f7f0c
DESKTOP-44PMP44\Słowik <e.slowinski92@gmail.com>
2024-07-22 15:48:11 +0200

    Initial commit with spring boot application

---
+++ src/test/java/com/github/repos_checker/ReposCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.repos_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
